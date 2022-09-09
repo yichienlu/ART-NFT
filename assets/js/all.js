0135460567b85da4e915865cb748e52dccb479d3
yichienlu <yichien.lu89@gmail.com>
2022-09-09 21:33:02 +0800

    Update 2022-09-09T13:32:56.766Z

---
+++ assets/js/all.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  }
+});
+
+function showSearchBar() {
+  document.querySelector('.search-bar').classList.add('d-flex');
+}
+
+function hideSearchBar() {
+  document.querySelector('.search-bar').classList.remove('d-flex');
+} // 瀑布流
+
+
+$('.artworks').imagesLoaded().progress(function () {
+  $('.artworks').masonry(); // 渲染整體畫面
+});
+//# sourceMappingURL=all.js.map
